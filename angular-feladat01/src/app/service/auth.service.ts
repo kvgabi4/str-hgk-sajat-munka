1c7993e37eba71229226c27c08e2a0aaaeeb1301
kvgabi4 <kvgabi4@gmail.com>
2021-08-02 17:42:53 +0200

    auth service - login

--- angular-feladat01/src/app/service/auth.service.ts
+++ angular-feladat01/src/app/service/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { User } from '../model/user';
 import { ConfigService } from './config.service';
 import { UserService } from './user.service';
@@ -14,25 +15,52 @@ export class AuthService {
   loginUrl = `${this.config.apiUrl}login`;
   logoutUrl = `${this.config.apiUrl}logout`;
   storageName = 'currentUser';
-  currentUserSubject: BehaviorSubject< User | null > = new BehaviorSubject<User | null>(null);
+  currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
   lastToken: string = '';
 
-constructor(
+  constructor(
     private config: ConfigService,
     private http: HttpClient,
     private router: Router,
     private userService: UserService,
-) {}
+  ) { }
 
-get currentUserValue(): User|null {
-  return this.currentUserSubject.value;
-}
+  get currentUserValue(): User | null {
+    return this.currentUserSubject.value;
+  }
 
-logout() {
-	localStorage.removeItem(this.storageName);
-	this.currentUserSubject.next(null);
-	this.router.navigate(['login']);
-}
+  logout() {
+    localStorage.removeItem(this.storageName);
+    this.currentUserSubject.next(null);
+    this.router.navigate(['login']);
+  }
+
+  login(loginData: User): Observable<User | User[] | null> {
+    return this.http.post<{ accessToken: string }>(
+      this.loginUrl,
+      { email: loginData.email, password: loginData.password }
+    )
+      .pipe(switchMap(response => {
+        if (response.accessToken) {
+          this.lastToken = response.accessToken;
+          return this.userService.query(`email=${loginData.email}`);
+        }
+        return of(null);
+      }))
+      .pipe(
+        tap(user => {
+          if (!user) {
+            localStorage.removeItem(this.storageName);
+            this.currentUserSubject.next(null);
+          } else {
+            (user as User[])[0].token = this.lastToken;
+            localStorage.setItem(this.storageName, JSON.stringify((user as User[])[0]));
+            this.currentUserSubject.next((user as User[])[0]);
+
+          }
+        })
+      );
+  }
 
 
 }
